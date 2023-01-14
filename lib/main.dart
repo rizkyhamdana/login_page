8f2872962e38b4e80d4de74dcee6199517275114
Rizky Hamdana <rizky.hamdana@gmail.com>
2023-01-14 21:14:58 +0700

    finishing

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_switch/flutter_switch.dart';
 import 'package:lottie/lottie.dart';
@@ -30,7 +31,20 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+class _MyHomePageState extends State<MyHomePage> with TickerProviderStateMixin {
+  late final AnimationController controller = AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 500),
+  )..forward();
+  late final Animation<double> _animation =
+      CurvedAnimation(parent: controller, curve: Curves.easeOut);
+  @override
+  void initState() {
+    super.initState();
+    controller;
+    AnimationController(vsync: this, duration: const Duration(seconds: 1));
+  }
+
   bool isDarkMode = false;
   LinearGradient lightGradient = const LinearGradient(
     begin: Alignment.topCenter,
@@ -53,119 +67,343 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height,
-        decoration:
-            BoxDecoration(gradient: isDarkMode ? darkGradient : lightGradient),
-        child: SingleChildScrollView(
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            height: MediaQuery.of(context).size.height,
-            padding: EdgeInsets.only(top: MediaQuery.of(context).padding.top),
-            child: Stack(
-              children: [
-                Positioned(
-                  bottom: MediaQuery.of(context).size.height / 2 - 48,
-                  left: 0,
-                  right: 0,
-                  child: isDarkMode
-                      ? Lottie.asset(
-                          'assets/anim/day.json',
-                          fit: BoxFit.cover,
-                          height: 200,
-                          width: MediaQuery.of(context).size.width,
-                        )
-                      : Lottie.asset(
-                          'assets/anim/night.json',
-                          fit: BoxFit.cover,
-                          height: 200,
-                          width: MediaQuery.of(context).size.width,
-                        ),
-                ),
-                Positioned(
-                    top: 56,
-                    right: 32,
-                    child: isDarkMode
-                        ? Lottie.asset('assets/anim/moon.json', height: 148)
-                        : Lottie.asset('assets/anim/sun.json', height: 148)),
-                isDarkMode
-                    ? Container()
-                    : Positioned(
-                        top: 120,
-                        left: 0,
-                        child: Lottie.asset(
-                          'assets/anim/cloud.json',
-                          width: MediaQuery.of(context).size.width,
-                        ),
-                      ),
-                isDarkMode
-                    ? Positioned(
-                        top: 0,
-                        left: 0,
-                        child: Lottie.asset(
-                          'assets/anim/shooting_star.json',
-                          width: MediaQuery.of(context).size.width,
-                        ),
-                      )
-                    : Container(),
-                Positioned(
-                    top: 16,
-                    right: 32,
-                    child: FlutterSwitch(
-                      onToggle: (bool value) {
-                        setState(() {
-                          isDarkMode = value;
-                        });
-                      },
-                      inactiveColor: Colors.white,
-                      activeColor: Colors.white,
-                      inactiveToggleColor: Colors.yellow,
-                      inactiveIcon: const Icon(
-                        Icons.sunny,
-                        color: Colors.white,
-                      ),
-                      activeIcon: const Icon(
-                        Icons.dark_mode_outlined,
-                        color: Colors.white,
-                      ),
-                      toggleColor: Colors.black,
-                      value: isDarkMode,
-                    )),
-                Positioned(
-                    bottom: 0,
-                    left: 0,
-                    right: 0,
-                    child: Container(
-                      height: MediaQuery.of(context).size.height / 2,
-                      padding: EdgeInsets.fromLTRB(
-                          32, 32, 32, MediaQuery.of(context).padding.bottom),
-                      decoration: BoxDecoration(
-                        color:
-                            isDarkMode ? const Color(0xff292E34) : Colors.white,
-                        borderRadius: const BorderRadius.only(
-                            topRight: Radius.circular(48)),
-                      ),
-                      child: Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
-                            children: const [
-                              Text('Username'),
-                            ],
-                          ),
-                          SizedBox(
-                              width: MediaQuery.of(context).size.width,
-                              child: ElevatedButton(
-                                  onPressed: () {}, child: const Text('Login')))
-                        ],
-                      ),
-                    )),
-              ],
-            ),
+      body: FadeTransition(
+        opacity: _animation,
+        child: Container(
+          width: MediaQuery.of(context).size.width,
+          height: MediaQuery.of(context).size.height,
+          decoration: BoxDecoration(
+              gradient: isDarkMode ? darkGradient : lightGradient),
+          child: SingleChildScrollView(
+            child: isDarkMode ? buildNightMode() : buildDayMode(),
           ),
         ),
+      ),
+    );
+  }
+
+  Widget buildDayMode() {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      padding:
+          EdgeInsets.only(top: MediaQuery.of(context).padding.top, bottom: 16),
+      child: Stack(
+        children: [
+          Positioned(
+            bottom: MediaQuery.of(context).size.height / 2 - 48,
+            left: 0,
+            right: 0,
+            child: Lottie.asset(
+              'assets/anim/night.json',
+              fit: BoxFit.cover,
+              height: 200,
+              width: MediaQuery.of(context).size.width,
+            ),
+          ),
+          Positioned(
+              top: 56,
+              left: 32,
+              child: Lottie.asset('assets/anim/sun.json', height: 148)),
+          Positioned(
+            top: 120,
+            left: 0,
+            child: Lottie.asset(
+              'assets/anim/cloud.json',
+              fit: BoxFit.cover,
+              width: MediaQuery.of(context).size.width,
+            ),
+          ),
+          Positioned(
+              top: 16,
+              right: 32,
+              child: FlutterSwitch(
+                onToggle: (bool value) {
+                  controller.reset();
+                  controller.forward();
+                  setState(() {
+                    isDarkMode = value;
+                  });
+                },
+                inactiveColor: Colors.white,
+                activeColor: Colors.white,
+                inactiveToggleColor: const Color(0xffFFD600),
+                inactiveIcon: const Icon(
+                  Icons.sunny,
+                  color: Colors.white,
+                ),
+                activeIcon: const Icon(
+                  Icons.dark_mode_outlined,
+                  color: Colors.white,
+                ),
+                toggleColor: Colors.black,
+                value: isDarkMode,
+              )),
+          Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                height: MediaQuery.of(context).size.height / 2,
+                padding: EdgeInsets.fromLTRB(
+                    32, 32, 32, MediaQuery.of(context).padding.bottom),
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius:
+                      BorderRadius.only(topRight: Radius.circular(48)),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          'Username',
+                          style: TextStyle(
+                            fontFamily: 'PlusJakartaSans',
+                            fontSize: 18,
+                            color: Color(0xff292E34),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        CupertinoTextField(
+                          padding: EdgeInsets.all(16),
+                          decoration: BoxDecoration(
+                            color: Color(0xffF0F0F0),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        Text(
+                          'Password',
+                          style: TextStyle(
+                            fontFamily: 'PlusJakartaSans',
+                            fontSize: 18,
+                            color: Color(0xff292E34),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        CupertinoTextField(
+                          padding: EdgeInsets.all(16),
+                          decoration: BoxDecoration(
+                            color: Color(0xffF0F0F0),
+                          ),
+                          suffix: Padding(
+                            padding: EdgeInsets.only(right: 16.0),
+                            child: SizedBox(
+                              width: 24,
+                              height: 24,
+                              child: Icon(Icons.visibility_off),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 4,
+                        ),
+                        Text(
+                          'Lupa password?',
+                          style: TextStyle(
+                            fontFamily: 'PlusJakartaSans',
+                            fontSize: 14,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.blue,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.blue,
+                          padding: const EdgeInsets.symmetric(vertical: 16),
+                        ),
+                        child: const Text(
+                          'Login',
+                          style: TextStyle(
+                              fontFamily: 'PlusJakartaSans',
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )),
+        ],
+      ),
+    );
+  }
+
+  Widget buildNightMode() {
+    return Container(
+      width: MediaQuery.of(context).size.width,
+      height: MediaQuery.of(context).size.height,
+      padding:
+          EdgeInsets.only(top: MediaQuery.of(context).padding.top, bottom: 16),
+      child: Stack(
+        children: [
+          Positioned(
+              bottom: MediaQuery.of(context).size.height / 2 - 48,
+              left: 0,
+              right: 0,
+              child: Lottie.asset(
+                'assets/anim/day.json',
+                fit: BoxFit.cover,
+                height: 200,
+                width: MediaQuery.of(context).size.width,
+              )),
+          Positioned(
+            top: 56,
+            right: 32,
+            child: Lottie.asset('assets/anim/moon.json', height: 148),
+          ),
+          Positioned(
+            top: 0,
+            left: 0,
+            child: Lottie.asset(
+              'assets/anim/shooting_star.json',
+              fit: BoxFit.cover,
+              height: MediaQuery.of(context).size.height / 2 - 180,
+            ),
+          ),
+          Positioned(
+              top: 16,
+              right: 32,
+              child: FlutterSwitch(
+                onToggle: (bool value) {
+                  controller.reset();
+                  controller.forward();
+                  setState(() {
+                    isDarkMode = value;
+                  });
+                },
+                inactiveColor: Colors.white,
+                activeColor: Colors.white,
+                inactiveToggleColor: const Color(0xffFFD600),
+                inactiveIcon: const Icon(
+                  Icons.sunny,
+                  color: Colors.white,
+                ),
+                activeIcon: const Icon(
+                  Icons.dark_mode_outlined,
+                  color: Colors.white,
+                ),
+                toggleColor: Colors.black,
+                value: isDarkMode,
+              )),
+          Positioned(
+              bottom: 0,
+              left: 0,
+              right: 0,
+              child: Container(
+                height: MediaQuery.of(context).size.height / 2,
+                padding: EdgeInsets.fromLTRB(
+                    32, 32, 32, MediaQuery.of(context).padding.bottom),
+                decoration: const BoxDecoration(
+                  color: Color(0xff292E34),
+                  borderRadius:
+                      BorderRadius.only(topRight: Radius.circular(48)),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: const [
+                        Text(
+                          'Username',
+                          style: TextStyle(
+                            fontFamily: 'PlusJakartaSans',
+                            fontSize: 18,
+                            color: Colors.white,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        CupertinoTextField(
+                          padding: EdgeInsets.all(16),
+                          decoration: BoxDecoration(
+                            color: Color(0xff1E2226),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        Text(
+                          'Password',
+                          style: TextStyle(
+                            fontFamily: 'PlusJakartaSans',
+                            fontSize: 18,
+                            color: Colors.white,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        CupertinoTextField(
+                          padding: EdgeInsets.all(16),
+                          decoration: BoxDecoration(
+                            color: Color(0xff1E2226),
+                          ),
+                          suffix: Padding(
+                            padding: EdgeInsets.only(right: 16.0),
+                            child: SizedBox(
+                              width: 24,
+                              height: 24,
+                              child: Icon(
+                                Icons.visibility_off,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(
+                          height: 4,
+                        ),
+                        Text(
+                          'Lupa password?',
+                          style: TextStyle(
+                            fontFamily: 'PlusJakartaSans',
+                            fontSize: 14,
+                            fontWeight: FontWeight.w700,
+                            color: Colors.white,
+                          ),
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: MediaQuery.of(context).size.width,
+                      child: ElevatedButton(
+                        onPressed: () {},
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.white,
+                          padding: const EdgeInsets.symmetric(vertical: 16),
+                        ),
+                        child: const Text(
+                          'Login',
+                          style: TextStyle(
+                              color: Color(0xff292E34),
+                              fontFamily: 'PlusJakartaSans',
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )),
+        ],
       ),
     );
   }
